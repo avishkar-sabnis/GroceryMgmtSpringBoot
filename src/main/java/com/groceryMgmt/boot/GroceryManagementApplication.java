2097f8f430e8a546983d1770d6f493fdf6016f56
avishkar sabnis <avishkarsabnis@gmail.com>
2022-07-02 16:39:21 +0530

    No Changes

---
+++ src/main/java/com/groceryMgmt/boot/GroceryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.groceryMgmt.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryManagementApplication.class, args);
+	}
+
+}
